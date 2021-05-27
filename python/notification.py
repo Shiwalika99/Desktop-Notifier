97fa580b6b65185bd4931abb72d7b958fae89a43
Shiwalika <82506220+Shiwalika99@users.noreply.github.com>
2021-05-27 13:48:24 -0700

    Add files via upload

---
+++ python/notification.py
@@ -0,0 +1,22 @@
+import time
+
+from plyer import notification
+
+
+
+if __name__ == "__main__":
+   while True :
+      notification.notify(
+      title = "Hey! It's Time to take rest",
+      message = " Sleep is a vital, often neglected, component of every person's overall health and well-being. Sleep is important because it enables the body to repair and be fit and ready for another day",
+       
+     
+       
+      timeout = 3
+       
+
+    )
+   time.sleep(10*10)
+  
+
+
